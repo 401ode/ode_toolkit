dc0a8da343c76e925d6c2ba6e7da43331e192fe9
Brian J. McGuirk <brian.mcguirk@ode.ri.gov>
2016-02-04 16:05:50 -0500

    Initial list, installer script.

---
+++ installer.ps1
@@ -0,0 +1,18 @@
+# Installer Scripts (Mostly via Chocolatey)
+#
+#
+#
+# # Meta-Tools
+# WGET
+choco install wget
+# Git
+choco install git
+
+# Browser - Essential
+choco install googlechrome
+choco install firefox
+
+# Data Analysis
+choco install miniconda3
+
+#
