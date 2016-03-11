853b3b25135fa3d0d4cb8198ea062810bfeb10b6
Brian J. McGuirk <brian.mcguirk@ode.ri.gov>
2016-03-11 10:46:53 -0500

    Small changes. Next branch to switch to Boxstarter.

--- installChocolatey.cmd
+++ installChocolatey.cmd
@@ -6,3 +6,4 @@ SET DIR=%~dp0%
 %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://chocolatey.org/install.ps1','install.ps1'))"
 ::run installer
 %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%install.ps1' %*"
+
--- installer.ps1
+++ installer.ps1
@@ -1,8 +1,11 @@
-# Installer Scripts (Mostly via Chocolatey)
+# Installer Scripts (Mostly via Chocolatey/Boxstarter)
 #
 #
-#
+# Boxstarter to manage installations.
+CINST Boxstarter
+
 # # Meta-Tools
+
 # WGET
 choco install wget
 # Git
@@ -12,7 +15,14 @@ choco install git
 choco install googlechrome
 choco install firefox
 
+# Microsoft - Databases
+# SQL Server Express
+
+# SQL Server Management Studio
+choco install mssqlservermanagementstudio2014express
+
 # Data Analysis
 choco install miniconda3
 
-#
+#System Tools
+choco install revo.uninstaller
