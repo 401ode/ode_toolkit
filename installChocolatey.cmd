98a456ef51d38e81f13748ac17390f1a346dfb13
Brian J. McGuirk <brian.mcguirk@ode.ri.gov>
2016-03-15 15:26:18 -0400

    Add start of boxstarter, more cool apps.

--- installChocolatey.cmd
+++ installChocolatey.cmd
@@ -7,3 +7,9 @@ SET DIR=%~dp0%
 ::run installer
 %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%install.ps1' %*"
 
+::Install Boxstarter
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& 'cinst boxstarter' %*"
+::Import Boxstarter chocolatey module
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& 'Import-Module Boxstarter.Chocolatey' %*"
+::Install published package.
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& 'Set-BoxstarterConfig -NugetSources "https://www.myget.org/F/401ode/api/v2' %*"
